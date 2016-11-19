0697ea0f261a91646e7469f799ff4deb3f7e6178
cormacwb <cormac.w.brennan@gmail.com>
2016-11-19 15:23:35 -0800

    Collect list of prominent frequencies

--- Mp3Reader/Program.cs
+++ Mp3Reader/Program.cs
@@ -1,74 +1,110 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NAudio;
+using NAudio.Dsp;
 using NAudio.Wave;
-using NAudio.Wave.SampleProviders;
 
 namespace Mp3Reader
 {
-    class Program
+    public class Program
     {
+        private const int BufferSize = 1024;
+        private const string Path = @"C:\Users\Cormac\Music\StationRipper\output\ECOMM Vancouver Scanner\tone.mp3";
+        const int TargetFrequency1 = 947;
+
         static void Main(string[] args)
         {
-            var path = @"C:\Users\Cormac\Music\StationRipper\output\ECOMM Vancouver Scanner\tone.mp3";
-            var output = "outputWithoutSilence.wav";
-            //Mp3ToWav(path, output);
-
-            //return;
-            float max = 0;
-            const float threshold = 3.05175781E-05F;
-            
-            using (var reader = new Mp3FileReader(path))
+            var prominentFrequencies = new List<int>();
+            using (var reader = new Mp3FileReader(Path))
             {
-                //var inputFormat = reader.WaveFormat;
                 var sampleProvider = reader.ToSampleProvider();
-                using (var writer = new WaveFileWriter(output, sampleProvider.WaveFormat))
+                var buffer = new float[BufferSize];
+
+                var bytesRead = 1;
+                while (true)
                 {
-                    WriteIntro(writer);
-
-                    Mp3Frame frame;
-
-                    var sig = new SignalGenerator();
-
-
+                    bytesRead = sampleProvider.Read(buffer, 0, buffer.Length);
                     
-                    float[] buffer = new float[10000];
-                    
-                    while (true)
-                    {
-                        int bytesRead = sampleProvider.Read(buffer, 0, buffer.Length);
-                        if (bytesRead <= 0)
-                            return;
+                    if (bytesRead < buffer.Length)
+                        break;
+                    if (buffer.All(n => Math.Abs(n) < 0.01)) continue;
 
-                        foreach (var sample in buffer)
-                        {
+                    var fft = CreateFftBuffer(buffer);
 
-                            if (sample > threshold)
-                            {
-                                writer.WriteSample(sample);
-                            }
-                        }
-                    }
-                    //foreach (var sample in buffer)
-                    //{
-                    //    if (sample > max)
-                    //    {
-                    //        max = sample;
-                    //    }
-                    //}
+                    FastFourierTransform.FFT(true, GetLog(), fft);
 
+                    prominentFrequencies.Add(GetMostProminentFrequency(fft, sampleProvider));
                 }
-
             }
+
+            var x = prominentFrequencies;
+            var y = x;
+        }
+
+        private static int GetLog()
+        {
+            return (int)Math.Log(BufferSize, 2);
+        }
+
+        private static int GetMostProminentFrequency(Complex[] fft, ISampleProvider sampleProvider)
+        {
+            var indexOfMaxMagnitude = GetIndexOfMaxMagnitude(fft);
+            return GetFrequency(indexOfMaxMagnitude, sampleProvider.WaveFormat.SampleRate, fft.Length);
+        }
+
+        private static int GetIndexOfMaxMagnitude(Complex[] fft)
+        {
+            double maxMagnitude = 0;
+            int indexOfMaxMagnitude = 0;
+            for (var i = 0; i < fft.Length; i++)
+            {
+                var magnitude = CalculateMagnitude(fft[i]);
+                if (magnitude > maxMagnitude)
+                {
+                    indexOfMaxMagnitude = i;
+                    maxMagnitude = magnitude;
+                }
+            }
+            return indexOfMaxMagnitude;
+        }
+
+        private static int GetFrequency(int indexOfMaxMagnitude, int sampleRate, int bufferSize)
+        {
+            return indexOfMaxMagnitude*sampleRate/bufferSize;
+        }
+
+        private static double CalculateMagnitude(Complex complex)
+        {
+            return Math.Sqrt((complex.X * complex.X) + (complex.Y * complex.Y));
+        }
+
+        private static Complex[] CreateFftBuffer(float[] buffer)
+        {
+            var fft = new Complex[BufferSize];
+            for (var i = 0; i < buffer.Length; i++)
+            {
+                var fftComplexInput = CreateComplexInput(buffer, i);
+                fft[i] = fftComplexInput;
+            }
+            return fft;
+        }
+
+        private static Complex CreateComplexInput(float[] buffer, int index)
+        {
+            var real = CreateRealPart(buffer, index);
+            const int imaginary = 0;
+
+            return new Complex {X = real, Y = imaginary};
+        }
+
+        private static float CreateRealPart(IReadOnlyList<float> buffer, int index)
+        {
+            return (float) (buffer[index]*FastFourierTransform.HammingWindow(index, BufferSize));
         }
 
         private static void WriteIntro(WaveFileWriter writer)
         {
-            var quietIntro = Enumerable.Repeat(0.2f, 20000).ToArray(); //new float[20000];
+            var quietIntro = Enumerable.Repeat(0.2f, 20000).ToArray();
 
             writer.WriteSamples(quietIntro, 0, quietIntro.Length);
         }
